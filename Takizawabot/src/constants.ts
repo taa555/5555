3f3e252ada011118ae13a3705e3f80f2a351fce7
taa555 <122363687+taa555@users.noreply.github.com>
2023-01-12 21:50:51 +0900

    Update constants.ts

--- Takizawabot/src/constants.ts
+++ Takizawabot/src/constants.ts
@@ -1,5 +1,5 @@
 export const DATA_FILE = "data.json";
-export const TARGET_USER_ID = "1613188972972503041";
+export const TARGET_USER_ID = "1488057653230469121";
 export const TWEET_DATE_FORMAT = "MM/dd HH:mm:ss";
 export const FETCH_INTERVAL = 15000;
 // Unlike FETCH_INTERVAL, this value should not be lowered as smaller value would cause CDN cache miss
